2d3d834ae2eefb4da8b29ef16c7780b68cc1034e
fer-2001 <fernandezmanuelalumno@gmail.com>
2022-10-16 11:35:47 -0300

    Implementacion mergeSort

--- practica-estructura/algoritmos/java/arraySorter/ArraySorter.java
+++ practica-estructura/algoritmos/java/arraySorter/ArraySorter.java
@@ -129,9 +129,62 @@ public class ArraySorter {
    * @param <T> el tipo de los elementos del arreglo, los cuales deben ser comparables entre sí
    * @param array el arreglo de los elementos a ordenar, no puede ser {@code null}
    */
-   public static <T extends Comparable<? super T>> void mergeSort(T[] array) {
-      throw new UnsupportedOperationException("TODO: implementar");    
-   }
+   public static void mergeSort(Integer[] inputArray) {
+       int inputLength = inputArray.length;
+       
+       if (inputLength < 2) {
+         return;
+       }
+       
+       int midIndex = inputLength / 2;
+       Integer[] leftHalf = new Integer[midIndex];
+       Integer[] rightHalf = new Integer[inputLength - midIndex];
+       
+       for (int i = 0; i < midIndex; i++) {
+         leftHalf[i] = inputArray[i];
+       }
+       for (int i = midIndex; i < inputLength; i++) {
+         rightHalf[i - midIndex] = inputArray[i];
+       }
+       
+       mergeSort(leftHalf);
+       mergeSort(rightHalf);
+       
+       merge(inputArray, leftHalf, rightHalf);
+    }
+
+  private static void merge (Integer[] inputArray, Integer[] leftHalf, Integer[] rightHalf) {
+    int leftSize = leftHalf.length;
+    int rightSize = rightHalf.length;
+    
+    int i = 0, j = 0, k = 0;
+    
+    while (i < leftSize && j < rightSize) {
+      if (leftHalf[i] <= rightHalf[j]) {
+        inputArray[k] = leftHalf[i];
+        i++;
+      }
+      else {
+        inputArray[k] = rightHalf[j];
+        j++;
+      }
+      k++;
+    }
+    
+    while (i < leftSize) {
+      inputArray[k] = leftHalf[i];
+      i++;
+      k++;
+    }
+    
+    while (j < rightSize) {
+      inputArray[k] = rightHalf[j];
+      j++;
+      k++;
+    }
+    
+  }
+
 
 
    /* (non-Javadoc)
