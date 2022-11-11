66b84d6ffc0bfd4087f18b698a3a27c7dd13f434
fer-2001 <fernandezmanuelalumno@gmail.com>
2022-11-11 16:35:51 -0300

    Ejercicio tipo parcial (rmMax)

--- Arboles-Haskell/Tree.hs
+++ Arboles-Haskell/Tree.hs
@@ -50,3 +50,45 @@ pertenece (N hi r hd) x = if r == x then True else (pertenece hi x) || (pertenec
 
 -- (N (N (N (N Nil 'A' Nil) 'B' (N Nil 'C' Nil)) 'D' (N Nil 'F' (N Nil 'G' Nil) )) 'H' (N (N (N Nil 'I' Nil) 'J' (N Nil 'K' Nil)) 'L' (N Nil 'N' (N Nil 'M' Nil))))
 -- (N (N Nil 2 Nil) 1 (N Nil 3 Nil))
+
+raiz :: (Tree a) -> a
+raiz (N hi r hd) = r
+
+borrar :: (Ord a) => a -> (Tree a) -> (Tree a)
+borrar x Nil = Nil
+borrar x (N hi r hd) 
+                    |(x < r) = (N (borrar x hi) r hd)
+                    |(x > r) = (N  hi r (borrar x hd))
+                    |(x == r) && hd /= Nil && hi == Nil = (N Nil (raiz hd) Nil)
+                    |(x == r) && hd == Nil && hi /= Nil = (N Nil (raiz hi) Nil)
+                    |(x == r) && hd /= Nil && hi /= Nil = (N (borrar (mayorValor hi) hi) (mayorValor hi) hd)
+                    |(x == r) && hd == Nil && hi == Nil = Nil
+
+mayorValor :: (Tree a) -> a
+mayorValor (N Nil r Nil) = r
+--mayorValor (N hi r Nil) = mayorValor hi
+mayorValor (N Nil r hd) = mayorValor hd
+mayorValor (N hi r hd) = mayorValor hd
+
+
+rmMax :: (Ord a) => (Tree a) -> (a, Tree a)
+rmMax (N hi r hd) = (mayor, (borrar mayor (N hi r hd)))
+                    where mayor = mayorValor (N hi r hd)
+
+
+{-
+-- Retorna un par en donde: el primer elemento es el máximo del ABB 
+--y el segundo es el arbol sin el maximo
+rmMax :: (Ord a) => (Tree a) -> (a, Tree a)
+rmMax Nil = error "Arbol vacio"
+rmMax (N hi r Nil) = (r, hi)
+rmMax (N hi r hd) = (m, (N hi r hd2))
+                 where (m, hd2) = rmMax hd
+
+rmMin :: (Ord a) => (Tree a) -> (a, Tree a)
+rmMin Nil = error "Arbol vacio"
+rmMin (N Nil r hd) = (r, hd)
+rmMin (N hi r hd) = (m, (N hi2 r hd))
+                 where (m, hi2) = rmMin hi
+-}
+-- (N (N (N Nil 20 Nil) 40 (N Nil 50 Nil)) 60 (N (Nil) 90 (N (Nil) 100 (Nil))))
